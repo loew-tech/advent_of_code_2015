3ec56923d4d3311c10b6b0874b542e167deb06a6
loew-tech <loew.technology@gmail.com>
2025-12-12 15:14:24 -0700

    day 2 pt. 2

--- classes.py
+++ classes.py
@@ -1,4 +1,25 @@
 from collections import namedtuple
 
 
-Box = namedtuple('Box', ['l', 'w', 'h'])
+# Box = namedtuple('Box', ['l', 'w', 'h'])
+
+class Box:
+
+    def __init__(self, l, w, h: int):
+        self.l, self.w, self.h = sorted([l, w, h])
+
+    @property
+    def surface_area(self):
+        return 2 * self.l * self.w + 2 * self.w * self.h + 2 * self.h * self.l
+
+    @property
+    def volume(self):
+        return self.l * self.w * self.h
+
+    @property
+    def wrapping_paper(self):
+        return self.surface_area + self.l * self.w
+
+    @property
+    def ribbon(self):
+        return 2 * (self.l + self.w) + self.volume
--- solutions.py
+++ solutions.py
@@ -1,5 +1,6 @@
 import inspect
 import sys
+from typing import List
 
 from classes import Box
 from utils import read_input
@@ -18,20 +19,20 @@ def day_1(part_1=True) -> int:
 
 
 def day_2(part_1=True) -> int:
-    def surface_area(b: Box) -> int:
-        return 2 * b.l * b.w + 2 * b.w * b.h + 2 * b.h * b.l
+    boxes: List[Box] = read_input(day=2,
+                                  parse=lambda ln: Box(*map(int,
+                                                            ln.split('x')))
+                                  )
 
-    def total_wrapping_paper(box_: Box):
-        sorted_ = sorted(box_)
-        return surface_area(box_) + sorted_[0] * sorted_[1]
-
-    boxes = read_input(day=2, parse=lambda ln: Box(*map(int, ln.split('x'))))
-    return sum(total_wrapping_paper(box) for box in boxes)
+    if part_1:
+        return sum(box.wrapping_paper for box in boxes)
+    return sum(box.ribbon for box in boxes)
 
 
 if __name__ == '__main__':
     args = (f'day_{i}' for i in (sys.argv[1:] if
-            sys.argv[1:] else range(1, 26)) if type(i) == int or i.isnumeric())
+                                 sys.argv[1:] else range(1, 26)) if
+            type(i) == int or i.isnumeric())
     members = inspect.getmembers(inspect.getmodule(inspect.currentframe()))
     funcs = {name: member for name, member in members
              if inspect.isfunction(member)}
